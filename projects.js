14209c608c35f61567ce8c9dbb6d772481027734
Andrés Lugo <andreslugo51@gmail.com>
2025-12-12 01:25:20 -0400

    Nuevo estilo

--- projects.js
+++ projects.js
@@ -24,6 +24,15 @@ projects = [
 
 $(document).ready(function(){
 
+    //Picture adjustment
+    picHeight = $('#picture')[0].offsetHeight
+    pictureExtra = (Math.sqrt( ((picHeight**2)*Math.cos(85*Math.PI/180)**2)/(1+Math.cos(85*Math.PI/180)**2)) )
+    console.log(pictureExtra)
+    
+    if (document.documentElement.clientWidth > 700){
+        $('#picture').css('right', `-${pictureExtra}px`)
+    }
+
     let projectCont = $('#project-container')
 
     for (let i in projects){
